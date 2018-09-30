6d29c87ece0fbaf379b80b83bd2db0dab4a98916
MickAvery <mav.cuyugan@gmail.com>
2018-09-29 21:41:11 -0700

    shell now working with test command csv

--- main.c
+++ main.c
@@ -6,6 +6,30 @@
 
 #include "ch.h"
 #include "hal.h"
+#include "shell.h"
+#include "chprintf.h"
+
+#include "pinconf.h"
+
+#define SHELL_WORKING_AREA_SIZE THD_WORKING_AREA_SIZE(2048)
+
+static void csv(BaseSequentialStream* chp, int argc, char* argv[])
+{
+  (void)argc;
+  (void)argv;
+  chprintf(chp, "Test\r\n");
+}
+
+static const ShellCommand shellcmds[] =
+{
+  {"csv", csv}
+};
+
+static const ShellConfig shellcfg =
+{
+  (BaseSequentialStream*)&SD4,
+  shellcmds
+};
 
 int main(void) {
 
@@ -13,7 +37,23 @@ int main(void) {
   halInit();
   chSysInit();
 
+  /* start serial */
+  palSetPadMode(UART_TX_PORT, UART_TX_PADNUM, PAL_MODE_ALTERNATE(UART_PIN_ALTMODE));
+  palSetPadMode(UART_RX_PORT, UART_RX_PADNUM, PAL_MODE_ALTERNATE(UART_PIN_ALTMODE));
+  sdStart(&SD4, NULL);
+
+  shellInit();
+
   while (1) {
+    thread_t* shelltp = chThdCreateFromHeap(
+      NULL,
+      SHELL_WORKING_AREA_SIZE,
+      "shell",
+      NORMALPRIO,
+      shellThread,
+      (void*)&shellcfg);
+
+    chThdWait(shelltp);
     chThdSleepMilliseconds(1000);
   }
 
